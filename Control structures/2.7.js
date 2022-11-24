6a79dd139b8c51eab4fa85604c9459599bae6668
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 10:47:15 +0100

    adding exercise 2.7

---
+++ Control structures/2.7.js
@@ -0,0 +1,13 @@
+let n = parseInt(prompt("Enter a number"));
+let sum = 0; 
+
+let i = 0;
+while (i < n){
+   let newNumber = parseInt(prompt("Enter a new number " + `${i+1}` ));
+   sum += newNumber;
+   i++;
+}
+
+alert(sum);
+
+
