b4c5ed5e877672821bbc9daadae4776065ee47cd
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 10:32:52 +0100

    Adding exercise 2.5

---
+++ Control structures/2.5.js
@@ -0,0 +1,7 @@
+
+   let userInput = parseInt(prompt("Give me your favorite number"));
+
+  while (userInput !== 42){
+    alert("Are you sure?");
+    userInput = parseInt(prompt("Give me your favorite number"));
+    }
