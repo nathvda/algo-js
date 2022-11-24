8bc020cc77b0190735095d8b510fc13d11b71b10
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 10:18:39 +0100

    adding exercise 2.3

---
+++ Control structures/2.3.js
@@ -0,0 +1,17 @@
+let b = 1;
+while (b <= 100){
+    let x = b%2;
+
+    if ( x == 0 ){
+        console.log(b);
+    }
+   b++;
+}
+
+for (let i = 1 ; i <= 50; i++){
+    let x = i%2;
+
+    if ( x == 0 ){
+        console.log(i);
+    }
+}
