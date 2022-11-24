58eea32e09b58c8e6f3c62f2623eaa72c3e47c55
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 10:38:08 +0100

    Adding exercise 2.6

---
+++ Control structures/2.6.js
@@ -0,0 +1,30 @@
+let numberChosen = prompt("Please, choose a number between 1 and 7");
+
+switch (numberChosen){
+
+   case "1":
+    alert("Monday");
+    break;
+    case "2": 
+    alert("Tuesday");
+    break;
+    case "3": 
+    alert("Wednesday");
+    break;
+    case "4": 
+    alert("Thursday");
+    break;
+    case "5": 
+    alert("Friday");
+    break;
+    case "6": 
+    alert("Saturday");
+    break;
+    case "7": 
+    alert("Sunday");
+    break;
+    default : 
+    alert("This isn't between 1 and 7");
+
+
+}
