198234915240e2c3c8ebff674258ccac11ff7126
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 11:57:57 +0100

    fixing 4.2 and creating 4.3

--- Functions/4.2.js
+++ Functions/4.2.js
@@ -5,7 +5,8 @@
  */
 function rand10(){
 
-    console.log(Math.floor(Math.random() * (10 - 1 + 1) + 1));
+    let randomNumber = (Math.floor(Math.random() * (10 - 1 + 1) + 1));
+    return randomNumber;
 }
 
-rand10();
+console.log(rand10());
