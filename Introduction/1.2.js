e5a681b39e9ce70055b81da2b800fe5f017a4151
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 09:38:22 +0100

    adding exercise 1.2

---
+++ Introduction/1.2.js
@@ -0,0 +1,7 @@
+let name = "Vanden Abeele";
+let firstName = "Nathalie"; 
+let city = "Charleroi";
+
+console.log("Your name is " + firstName + " " + name + " and you live in " + city);
+
+console.log(`Your name is ${firstName} ${name} and you live in ${city}`);
