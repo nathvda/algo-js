78ad2b48e9f65ad9d1bc9f854e26c4b3103374a2
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-24 11:09:24 +0100

    Adding exercise 3.2

---
+++ Arrays/3.2.js
@@ -0,0 +1,19 @@
+let arrayTest = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for(let elem of arrayTest){
+    sum += elem;
+}
+
+let Average = sum/(arrayTest.length);
+console.log(Average);
+
+arrayTest = [100, 101, 102];
+sum = 0;
+
+for(let elem of arrayTest){
+    sum += elem;
+}
+
+Average = sum/(arrayTest.length);
+console.log(Average);
