17cc212b0fa15867da57c550afa2ba87a886779d
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-25 14:13:19 +0100

    Adding the Space Age bonus.

---
+++ assets/Bonuses/SpaceAge.js
@@ -0,0 +1,49 @@
+
+function spaceAge(){
+
+let seconds = Number(prompt("enter a number of seconds"));
+let toYears = seconds / 60 / 60 / 24 / 365.25;
+let chosenPlanet = prompt("choose a planet");
+
+
+
+    let planets = {
+        "Mercury": "0.2408467",
+        "Venus": "0.61519726",   
+        "Earth": "1",
+        "Mars": "1.8808158",
+        "Jupiter": "11.862615"
+    }
+    
+        switch (chosenPlanet){
+
+            case "Mercury": 
+            alert(`You chose ${chosenPlanet}`);
+            alert(`Your space Age is : ${(Number(planets[chosenPlanet]) * toYears).toFixed(1)} years old.`);
+            break;
+
+            case "Venus": 
+            alert(`You chose ${chosenPlanet}`);
+            alert(`Your space Age is : ${(Number(planets[chosenPlanet]) * toYears).toFixed(1)} years old.`);
+            break;
+
+            case "Earth": 
+            alert(`You chose ${chosenPlanet}`);
+            alert(`Your space Age is : ${(Number(planets[chosenPlanet]) * toYears).toFixed(1)} years old.`);
+            break;
+
+            case "Mars": 
+            alert(`You chose ${chosenPlanet}`);
+            alert(`Your space Age is : ${(Number(planets[chosenPlanet]) * toYears).toFixed(1)} years old.`);
+            break;
+
+            case "Jupiter": 
+            alert(`You chose ${chosenPlanet}`);
+            alert(`Your space age is : ${(Number(planets[chosenPlanet]) * toYears).toFixed(1)} years old.`);
+            break;
+
+            default:
+            alert(`You didn't chose a planet... Are you from Pluton ?`);
+
+        }
+}
