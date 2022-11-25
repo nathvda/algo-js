d1e89194037dc8743814fcb6fadb85ef4d2307c8
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-11-25 16:14:33 +0100

    changing calculator

--- assets/Bonuses/ChangeCalculator.js
+++ assets/Bonuses/ChangeCalculator.js
@@ -5,8 +5,7 @@ function computeChange(price, moneyHanded){
 
     let changeGiven = [];
 
-    let bankNotes = [500,200,100,50,20,10,5];
-    let euroCoins = [2,1,0.5,0.2,0.1,0.05,0.02,0.01];
+    let bankNotes = [500,200,100,50,20,10,5,2,1,Number(0.5),Number(0.2),Number(0.1),Number(0.05),Number(0.02),Number(0.01)];
     
 
     if( price > moneyHanded || price < 0 || moneyHanded < 0 || isNaN(price) || isNaN(moneyHanded) ){
@@ -16,47 +15,62 @@ function computeChange(price, moneyHanded){
 
     } else {
 
-        let changeDue = moneyHanded - price;
+        let changeDue = (moneyHanded - price).toFixed(2);
         console.log(changeDue);
 
         let i = 0;
-
-
-        while(changeDue > 0 || i < 6){
+        
+        while(changeDue > 0){
 
                 let next;
-                if (i < 6){
-                    next = `${bankNotes[i+1]} bills`;
-                } else {
-                    next = "coins.";
-                }
+                    if (i < 6){
+                        next = `${bankNotes[i+1]} bills`;
+                        current =  "bills";
+                    } else {
+                        next = `${bankNotes[i+1]} coins`;
+                    }
 
 
              if ((changeDue % bankNotes[i]) == 0){
+
                 let amount = Number(changeDue / bankNotes[i]); 
                 changeGiven.push(amount);
                 changeDue -= amount * bankNotes[i];
-                return changeGiven;
-             } else {
-                let amount = Number(changeDue / bankNotes[i]).toFixed(0); 
-                console.log(amount);
-                console.log(changeGiven);
 
-                if( (amount * bankNotes[i]) > changeDue ){ 
-                    changeDue -= (amount-1) * bankNotes[i];
-                    console.log(`Not enough for change, let's try ${next}.`);
-                    changeGiven.push(amount-1);
-                } else if (amount == 0) {
-                     console.log(`Looks like I can't give you any ${bankNotes[i]} bills for that`);
-                }  else {
-                    changeDue -= amount * bankNotes[i];
-                    console.log(`Here's your change, how much more do I owe you ? I'll go to ${next}`);
-                    changeGiven.push(amount);
-                }     
-                
-                console.log(changeDue);
+                return changeGiven;
+
+             } else {
+
+                    let amount = Number(changeDue / bankNotes[i]).toFixed(0); 
+                    console.log(amount);
+                    console.log(changeGiven);
+
+                    if( (amount * bankNotes[i]) > changeDue ){ 
+                            changeDue -= (amount-1) * bankNotes[i];
+                            console.log(`Not enough for change, let's try ${next}.`);
+                            changeGiven.push(amount-1);
+
+                        }   else   if ((amount * bankNotes[i]) == changeDue ) {
+                            console.log(`... ${bankNotes[i]} ${current} for that`);
+                            changeDue -= amount * bankNotes[i];
+                            changeGiven.push(amount);
+    
+                           } 
+                        
+                        else if (amount == 0) {
+                            console.log(`Looks like I can't give you any ${bankNotes[i]} ${current} for that`);
+                            changeGiven.push(amount);
+
+                        } else {
+                            console.log(`Here's your change, how much more do I owe you ? I'll go to ${next}`);
+                            changeGiven.push(amount);
+                            changeDue -= amount * bankNotes[i];
+                        }     
+                   
              }
-i++
+        console.log(changeDue);         
+        i++;
+        
         }
       
         return;
